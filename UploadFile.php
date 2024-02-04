89e03a344ea262cfc832453f3cd44018d376493a
LABMATT <matt42764@gmail.com>
2024-02-04 17:06:10 +1100

    Database Backup
    - Copys over a database every time a chage is made. these are stored in the backup database folder then delected after x amout of time.
    - deletion period is enterable

--- UploadFile.php
+++ UploadFile.php
@@ -1,7 +1,10 @@
 <?php
-// Amout of days a backup is kept before been deleted. 
-$databaseBackupKeepedDay = 60;
+// Amout of Seconds a backup is kept before been deleted. 5184000 is 60 days
+$databaseBackupKeepedSeconds = 60;
 
+//copy("test.txt","test2.txt");
+
+clearBackup($databaseBackupKeepedSeconds);
 BackupDatabase();
 
 // if database does not exist then create database.
@@ -11,6 +14,7 @@ function CreateDatabase()
     
 }
 
+// Database backup copys the database file to anotehr folder.
 function BackupDatabase() {
     
     // Returns a unix timestamp for rile name. 
@@ -18,12 +22,56 @@ function BackupDatabase() {
     
     // set the new database names
     $dbfile = 'database.bin';
-    $newdbfile = DIRECTORY_SEPARATOR . 'DatabaseBackups' . DIRECTORY_SEPARATOR  . 'Database_Backup_unixts_' . $unixTimestamp . '.bin';
-    
+    $newdbfile = 'DatabaseBackups' . DIRECTORY_SEPARATOR  . 'Database_Backup_unixts_' . $unixTimestamp . '.bin';
+
+
     // Copy the old database over to the backups folder.
     if (!copy($dbfile, $newdbfile)) {
-        echo "Failed to copy old database file to backups folder. \n";
+        echo "Failed to copy old database file to backups folder.";
     }
 }
+
+
+function clearBackup($databaseBackupKeepedSeconds) {
+
+    $backupFiles = scandir('DatabaseBackups');
+
+    //print_r($backupFiles);
+
+    if($backupFiles) {
+
+        foreach($backupFiles as $key => $filename) {
+
+            if (file_exists('DatabaseBackups' . DIRECTORY_SEPARATOR . $filename)) {
+
+                // If the file is longer than allowerble backup time then delete it.
+                if($filename != '..' & $filename != '.') {
+                    echo "checking file $filename <br>";
+                    echo "$filename was last modified: " . filemtime($filename) . "<br>";
+
+                    echo time() . " normal time <br>";
+                    echo filemtime('DatabaseBackups' . DIRECTORY_SEPARATOR . $filename) . "file time <br>";
+                    echo (time() - filemtime('DatabaseBackups' . DIRECTORY_SEPARATOR . $filename));
+
+                    if((time() - filemtime('DatabaseBackups' . DIRECTORY_SEPARATOR . $filename)) > $databaseBackupKeepedSeconds) {
+                        unlink('DatabaseBackups' . DIRECTORY_SEPARATOR . $filename);
+                    }
+                }
+
+            } else {
+                echo "$filename no exit: <br>";
+            }
+        }
+    }
+
+
+
+    /*
+
+    */
+
+}
+
+
 
 ?> 
