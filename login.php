dd3b1fdefeedcca05ebfa70779e6a347e88f6ee4
LABMATT <matt42764@gmail.com>
2024-04-04 15:19:41 +1100

    Remade Login Screen

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
     <h2 id="title">Sign In</h2>
         <form action="/action_page.php">
             <input type="text" id="username" name="username" placeholder="Username"><br>
-            <input type="text" id="password" name="password" placeholder="Password"><br><br>
+            <input type="password" id="password" name="password" placeholder="Password"><br><br>
             <input id="submit" type="submit" value="Sign In">
         </form> 
     <p id="msg">Error Loging In</p>
