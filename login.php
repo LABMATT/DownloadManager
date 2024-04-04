dea446fa4df50fb616509c176b23f05eae7afd28
LABMATT <matt42764@gmail.com>
2024-04-04 14:51:03 +1100

    Login page

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    <!-- Inportnet info -->
+    <title>Sign In</title>
+    <meta charset="UTF-8">
+
+    <!-- Include nessary script files. -->
+    <script type="text/javascript" src="assets\javascript\failedLogin.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets\styles\login.css">
+
+
+</head>
+
+<!-- Main Html Body -->
+    <body>
+
+    
+
+</body>
+</html>
