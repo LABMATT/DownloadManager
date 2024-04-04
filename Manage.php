4ae3f9d4de9b9a25693e7604373585e46f39fd3e
LABMATT <matt42764@gmail.com>
2024-04-04 15:35:59 +1100

    Download Manger Front end
    - Added page and headder

---
+++ Manage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    <!-- Inportnet info -->
+        <title>Manage</title>
+        <meta charset="UTF-8">
+
+    <!-- Include nessary script files. -->
+        <script type="text/javascript" src="assets\javascript\failedLogin.js"></script>
+        <link rel="stylesheet" type="text/css" href="assets\styles\Manger.css">
+    </head>
+
+<!-- Main Html Body -->
+    <body>
+    <div id="header">
+        <button class="HeaderButton">New Download</button>
+        <button class="HeaderButton">Disable Selected</button>
+        <button class="HeaderButton">Refresh Manifests</button>
+        <button class="HeaderButton">Sign Out</button>
+    </div>
+        
+
+    </body>
+</html>
--- login.php
+++ login.php
@@ -16,12 +16,12 @@
     
     <div id="LoginDiv">
     <h2 id="title">Sign In</h2>
-        <form action="/action_page.php">
+        <form action="/Mange.php" method="post">
             <input type="text" id="username" name="username" placeholder="Username"><br>
             <input type="password" id="password" name="password" placeholder="Password"><br><br>
             <input id="submit" type="submit" value="Sign In">
         </form> 
-    <p id="msg">Error Loging In</p>
+    <p id="msg"></p>
     </div>
     
     </body>
